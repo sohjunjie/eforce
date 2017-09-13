93a586af05797ab8c0a5e957a0b895ec66e166c7
sohjunjie <junjie.soh93@gmail.com>
2017-09-13 21:23:49 +0800

    modify layout

--- eforce_front/urls.py
+++ eforce_front/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?i)home/$', views.go_to_homepage, name="ef-home"),
     url(r'^(?i)home/logout/$', views.logout_user, name="ef-logout"),
 
-    url(r'^(?i)home/efassets/$', views.go_to_manage_ef_assets_page, name="ef-efassets"),
+    url(r'^(?i)efassets/manage/$', views.go_to_manage_ef_assets_page, name="ef-efassets-manage"),
 
 ]
