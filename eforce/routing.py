9d1f91081d559b2a7fbeade934f3207e27e33fdc
sohjunjie <junjie.soh93@gmail.com>
2017-10-22 23:10:02 +0800

    modify eforce for airlineapp

--- eforce/routing.py
+++ eforce/routing.py
@@ -1,14 +1,22 @@
 from channels.routing import route
-from sw_layer.consumers import ws_connect_efhq, ws_connect_efassets, ws_disconnect_hq, ws_disconnect_efassets
 
+from sw_layer.consumers import ws_connect_efhq, ws_connect_efassets, ws_disconnect_hq, \
+    ws_disconnect_efassets
+
+# FOR AIRLINEAPP
+from sw_layer.consumers import ws_connect_airlinechat, ws_disconnect_airlinechat, \
+    ws_receive_airlinechat
 
 channel_routing = [
     route("websocket.connect", ws_connect_efhq, path=r'^/efhq/$'),
-
     route("websocket.connect", ws_connect_efassets, path=r'^/efassets/$'),
-
     route("websocket.disconnect", ws_disconnect_hq, path=r'^/efhq/$'),
     route("websocket.disconnect", ws_disconnect_efassets, path=r'^/efassets/$'),
 
+
+    # FOR AIRLINEAPP
+    route("websocket.connect", ws_connect_airlinechat, path=r'^/airlineapp/chat/$'),
+    route("websocket.receive", ws_receive_airlinechat, path=r'^/airlineapp/chat/$'),
+    route("websocket.disconnect", ws_disconnect_airlinechat, path=r'^/airlineapp/chat/$'),
 
 ]
--- sw_layer/consumers.py
+++ sw_layer/consumers.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import User
 
 from eforce.settings import EF_HQ_ROLENAME
 
+# FOR AIRLINEAPP
+import json
+AIRLINEAPP_TRACKER = {}
+
 
 @channel_session_user_from_http
 def ws_connect_efhq(message):
@@ -35,3 +39,41 @@ def ws_disconnect_hq(message):
 @channel_session_user_from_http
 def ws_disconnect_efassets(message):
     Group('efassets').discard(message.reply_channel)
+
+
+# FOR AIRLINEAPP
+@channel_session
+def ws_connect_airlinechat(message):
+    Group("airlineapp").add(message.reply_channel)
+    numPeople = len(Group("airlineapp").channel_layer.group_channels("airlineapp"))
+    Group("airlineapp").send({
+        "text": json.dumps({
+            "usersConnected": numPeople
+        })
+    })
+
+
+@channel_session
+def ws_receive_airlinechat(message):
+    sessKey = message.channel_session.session_key
+    chatUser = json.loads(message['text'])
+    AIRLINEAPP_TRACKER[sessKey] = chatUser
+    Group("airlineapp").send({
+        "text": json.dumps({
+            "chatUsersDetails": AIRLINEAPP_TRACKER
+        })
+    })
+
+
+@channel_session
+def ws_disconnect_airlinechat(message):
+    sessKey = message.channel_session.session_key
+    Group('airlineapp').discard(message.reply_channel)
+    del AIRLINEAPP_TRACKER[sessKey]
+
+    numPeople = len(Group("airlineapp").channel_layer.group_channels("airlineapp"))
+    Group("airlineapp").send({
+        "text": json.dumps({
+            "peopleConnected": numPeople
+        })
+    })
