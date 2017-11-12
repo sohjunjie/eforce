8f4e47838acac0e00fcf216050af8bb4a06d180f
sohjunjie <junjie.soh93@gmail.com>
2017-11-12 21:56:37 +0800

    include environables in new html

--- eforce_front/static/pages/ef_hq/base.script.js
+++ eforce_front/static/pages/ef_hq/base.script.js
@@ -1,7 +1,3 @@
-// DEFINE BASE EF HQ CONST
-var CONST_CMO_DOMAIN = "//220.255.94.107:628/";
-var CONST_THIS_READABLE_USER_GROUP = "EF HQ";
-
 $(document).ready(function() {
 
   if(location.protocol == 'https:'){
